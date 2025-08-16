e9aad3a7758d67b455e7f01762ebe61cb918239a
Marcus Chandra <mzxchandra@gmail.com>
2025-08-16 16:43:06 -0500

    psycopg2 compatibility fix

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ set -o errexit
 # Upgrade pip to latest version
 pip install --upgrade pip
 
-# Install dependencies
-pip install -r requirements.txt
+# Install dependencies with verbose output for debugging
+pip install -r requirements.txt --verbose
 
 # Create database tables (will run once on first deploy)
 python init_prod_db.py
